a5c7eb678cef882fd5bc07681674d60d52f07646
felicien.courdesse@ynov.com <felicien.courdesse@ynov.com>
2023-03-09 18:00:45 +0100

    Update
    Work with background window

--- ChatAnalyzer.py
+++ ChatAnalyzer.py
@@ -6,6 +6,9 @@ import pyautogui
 import pytesseract
 import win32gui
 import re
+import win32gui
+import win32ui
+import win32.lib.win32con as win32con
 from termcolor import colored
 
 window_title = "Dofus Chat - Thegoodguy-[Ilz]"
@@ -22,64 +25,116 @@ chatColor = {
     "": "white"
 }
 
-
-def is_window_on_foreground(window_title):
-    # Get handle of the currently active window
-    foreground_window = win32gui.GetForegroundWindow()
-
-    # Get handle of the window you're interested in
-    window_handle = win32gui.FindWindow(None, window_title)
-
-    # Compare the handles
-    return foreground_window == window_handle
+class Message:
+    def __init__(self, User, Time, Content, Type):
+        self.User = User
+        self.Time = Time
+        self.Content = Content
+        self.Type = Type
+    
+    def toString(self):
+        return "{self.Time} {self.User} {self.Content}"
 
 
 def update_screen():
 
     text = ""
     window = pyautogui.getWindowsWithTitle(window_title)[0]
-    window.activate()
+    hwnd = win32gui.FindWindow(None, "Dofus Chat - Thegoodguy-[Ilz]")
+
     while True:
-        left, top, width, height = window.left, window.top, window.width, window.height
-        screenshot = pyautogui.screenshot(
-            region=(left+10, top+65, width-20, height-80))
-        screenshot = np.array(screenshot)
-        screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
+        left, top, width, height = window.left, window.top, window.width-20, window.height-20
+        screenshot = background_screenshot(hwnd, width, height)
 
-        # Perform OCR on the screenshot
-        if is_window_on_foreground:
-            text1 = pytesseract.image_to_string(Image.fromarray(screenshot))
+        bitmap_image = Image.fromarray(screenshot)
 
-            # Print the recognized text
+        # Convert to grayscale
+        grayscale_image = bitmap_image.convert('RGB')
+        text1 = pytesseract.image_to_string(grayscale_image)
+        # Print the recognized text
+        if text != text1:
+            print("---------------------")
+            text = text1
+            tab = list_to_message(text.split("\n"))
+            for line in tab:
+                print(line)
+            # tab = re.sub("[\[]\d\d.\d\d.\d\d[\]]",
+            #              "\n", text.replace("\n", "")).split("\n")
+            # for line in tab:
+            #     print(colored(line, chatColor.get(
+            #         line.lstrip().split(" ", 1)[0])))
 
-            if text != text1:
-                print("---------------------")
-
-                tab = re.sub("[\[]\d\d.\d\d.\d\d[\]]",
-                             "\n", text.replace("\n", "")).split("\n")
-                for line in tab:
-                    print(colored(line, chatColor.get(
-                        line.lstrip().split(" ", 1)[0])))
-                text = text1
 
         # Show area
-        cv.imshow("Computer Vision", screenshot)
+        # cv.imshow("Computer Vision", screenshot)
         key = cv.waitKey(10)
         if key == ord('q'):
             break
 
+def return_categorie(s):
+    s.lstrip().split(" ", 1)[0]
+    if "(/t)"in s:
+        return "equipe"
+    elif "(/g)"in s:
+        return "guilde"
+    elif "(/a)" in s:
+        return "alliance"
+    elif "(/p)" in s:
+        return "groupe"
+    elif "(/b)" in s:
+        return "commerce"
+    elif "(/r)" in s:
+        return "recrutement"
+    elif "de" in s:
+        return "prive"
+    elif "(/c" in s:
+        return "communaute"
+    elif "(info)" in s:
+        return "info"
+    elif "(combat)" in s:
+        return "combat"
+    else:
+        return "general"
+
+
+def list_to_message(s):
+    messages = []
+    for line in s:
+        if re.compile("[\[]\d\d.\d\d.\d\d[\]]").search(line):
+            time = line[:10]
+            line = re.sub("[\[]\d\d.\d\d.\d\d[\]]","", line)
+            pattern = line.lstrip().split(" ", 1)[0]
+            categorie = return_categorie(pattern)
+            if categorie == "communaute":
+                for _ in range(2):
+                    line = line.replace(line.lstrip().split(" ", 1)[0], "")
+            else:
+                line = line.replace(line.lstrip().split(" ", 1)[0], "")
+            
+            print(line)
+
+    return messages
+
+
+
+
+            
+
+def background_screenshot(hwnd, width, height):
+    wDC = win32gui.GetWindowDC(hwnd)
+    dcObj = win32ui.CreateDCFromHandle(wDC)
+    cDC = dcObj.CreateCompatibleDC()
+    dataBitMap = win32ui.CreateBitmap()
+    dataBitMap.CreateCompatibleBitmap(dcObj, width, height)
+    cDC.SelectObject(dataBitMap)
+    cDC.BitBlt((-10,-65),(width, height) , dcObj, (0,0), win32con.SRCCOPY)
+
+    # Get the bitmap data as a numpy array
+    data = dataBitMap.GetBitmapBits(True)
+    bitmap_array = np.frombuffer(data, dtype=np.uint8)
+    bitmap_array = np.reshape(bitmap_array, (height, width, 4))[:,:,2::-1]
+    return bitmap_array
 
 if __name__ == "__main__":
     update_screen()
 
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
--- TestRecord.py
+++ TestRecord.py
@@ -1,42 +1,18 @@
-from PIL import Image, ImageGrab
-import numpy as np
-import cv2 as cv
-import time
-import pyautogui
-import win32gui
-import win32api
+from scapy.all import *
+from scapy.layers.inet import TCP
 
 
-def update_screen():
-    t0 = time.time()
-    while True:
-        window_title = "Dofus Chat - Thegoodguy-[Ilz]"
-        hwnd = win32gui.FindWindow(None, window_title)
-        if hwnd:
-            monitor = win32api.MonitorFromWindow(hwnd)
-            monitor_info = win32api.GetMonitorInfo(monitor)
-            left, top, right, bottom = monitor_info["Monitor"]
-            screenshot = pyautogui.screenshot(region=(left+100, top, right, bottom))
-            window_rect = win32gui.GetWindowRect(hwnd)
-            window_left, window_top, window_right, window_bottom = window_rect
-            window_width = window_right - window_left
-            window_height = window_bottom - window_top
-            crop_left = window_left - left
-            crop_top = window_top - top
-            crop_right = crop_left + window_width
-            crop_bottom = crop_top + window_height
-            screenshot = screenshot.crop((crop_left, crop_top, crop_right, crop_bottom))
-            screenshot = np.array(screenshot)
-            screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
+# Define a function to capture and process packets
+def process_packet(packet):
+    if packet.haslayer(TCP) and packet.haslayer(Raw):
+        src_port = packet[TCP].sport
+        dst_port = packet[TCP].dport
+        if dst_port == 5555 and 'POST' in str(packet[TCP].payload):
+            # Dofus chat message found
+            chat_message = str(packet[TCP].payload)
+            # Extract the chat message from the payload
+            chat_message = chat_message.split('\\r\\n')[-1].strip()
+            print(chat_message)
 
-            cv.imshow("Computer Vision", screenshot)
-            key = cv.waitKey(10)
-            if key == ord('q'):
-                break
-            ex_time = time.time() - t0
-            print("FPS: " + str(1 / (ex_time)))
-            t0 = time.time()
-
-
-if __name__ == "__main__":
-    update_screen()
+# Start capturing packets
+sniff(filter='tcp', prn=process_packet)
